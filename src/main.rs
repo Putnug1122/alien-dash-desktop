4c3475bddf899f7dbf64d409f45be77b735da26a
Putra Nugraha <asiapatta10@gmail.com>
2023-04-11 08:31:05 +0800

    initial commit and first asset

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+
+use bevy::prelude::*;
+
+// region       -- Asset Constant
+
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+
+// end region
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Rust Invaders".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run()
+}
+
+fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load(PLAYER_SPRITE),
+        ..Default::default()
+    });
+}
